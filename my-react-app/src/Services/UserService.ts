483f50d3619ab7efcc9e3be25bb6f14fd6686c75
RushwithaNampelly <nampallyrushwitha5@gmail.com>
2025-09-14 00:33:48 +0530

    integrated

--- my-react-app/src/Services/UserService.ts
+++ my-react-app/src/Services/UserService.ts
@@ -2,7 +2,7 @@ import type { SellerRegisterRequest } from "../Type/SellerRegisterRequest";
 import API from "./Api";
 
 const AuthService = {
-  registerSeller: (data: SellerRegisterRequest) => API.post("/auth/register-seller", data),
+  registerSeller: (data: SellerRegisterRequest) => API.post("auth/register-seller", data),
   login: (data: any) => API.post("/auth/login", data)
 };
 
