34b26af498d63317153d5dd62d08ce5250cc32b2
sathwik-1804 <valgundawarsunnysathwik@gmail.com>
2025-09-13 18:53:13 +0530

    ok

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,19 +1,27 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Signup from "./pages/SignUp";
+// import React from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Home from "./pages/Home";
+// import Login from "./pages/Login";
+// import Signup from "./pages/SignUp";
 
-const App: React.FC = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-    </Router>
-  );
-};
+// const App: React.FC = () => {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/login" element={<Login />} />
+//         <Route path="/signup" element={<Signup />} />
+//       </Routes>
+//     </Router>
+//   );
+// };
+
+// export default App;
+
+import Orders from "./pages/Orders";
+
+function App() {
+  return <Orders />;
+}
 
 export default App;
