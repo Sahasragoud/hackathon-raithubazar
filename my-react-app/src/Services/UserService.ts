19dacb911ad5db9ee8bec51411f6f414cbfeef10
sathwik-1804 <valgundawarsunnysathwik@gmail.com>
2025-09-14 11:49:24 +0530

    df

--- my-react-app/src/Services/UserService.ts
+++ my-react-app/src/Services/UserService.ts
@@ -7,4 +7,15 @@ const AuthService = {
   registerSeller: (data: any) => API.post(`/auth/register-seller`, data)
 };
 
+const AdminService = {
+  getAllUsers : (page : number, size:number, sortField: string, sortDirection: string) => API.get(`/admin/users?page=${page}&size=${size}&sortField=${sortField}&sortDirection=${sortDirection}`),
+  getAllOrders : (page : number, size:number, sortField: string, sortDirection: string) => API.get(`/admin/orders?page=${page}&size=${size}&sortField=${sortField}&sortDirection=${sortDirection}`),
+  getAllProducts : (page : number, size:number, sortField: string, sortDirection: string) => API.get(`/admin/products?page=${page}&size=${size}&sortField=${sortField}&sortDirection=${sortDirection}`),
+  deleteUser : (userId: number) => API.delete(`/admin/users/${userId}`),
+  deleteProduct : (productId: number) => API.delete(`/admin/products/${productId}`)
+}
+
+const OrderService = {
+  
+}
 export default AuthService;
